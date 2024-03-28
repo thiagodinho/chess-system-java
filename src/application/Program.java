579eb4b1e934a39c1d42796874cbcef592a3c87a
Thiago Antunes <thiagodinhoan@gmail.com>
2024-03-28 11:55:07 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("oi");
+		Position p1 = new Position(3, 5);
+		System.out.println(p1);
 	}
 
 }
