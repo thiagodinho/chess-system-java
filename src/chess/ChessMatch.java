d38b93bab5b4c0588780948f8d87c8174decae6a
Thiago Antunes <thiagodinhoan@gmail.com>
2024-03-28 15:14:26 -0300

    BoardException and defensive programming

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -27,6 +27,7 @@ public class ChessMatch {
 		board.placePiece(new Rook(board, Color.WHITE), new Position(2,1));
 		board.placePiece(new King(board, Color.BLACK), new Position(0,4));
 		board.placePiece(new King(board, Color.WHITE), new Position(7,4));
+		
 	}
 	
 }
